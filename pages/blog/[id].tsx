d5df35f08859f6994ccd3385aa5556a6fe537cb8
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-10-01 21:37:48 +0900

    initial commit.

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,48 @@
+import { GetStaticPaths } from 'next';
+import { client } from '../../libs/client';
+import type { Blog } from '../../types/blog'
+
+interface Props {
+  blog: Blog;
+};
+
+interface Context {
+  params: { id: string },
+  locales: any,
+  locale: any,
+  defaultLocale: any
+};
+
+export default function BlogId({ blog }: Props) {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content: any) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context: Context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
+import type { NextPage, InferGetStaticPropsType } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { client } from '../libs/client'
+import type { Blog, Tag } from '../types/blog'
 
-const Home: NextPage = () => {
+type Props = {
+  blogs: Blog[];
+  tags: Tag[];
+}
+
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  blogs,
+  tags,
+}: Props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,39 +28,16 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <ul>
+            {blogs.map((blog) => (
+              <li key={blog.id}>
+                <Link href={`/blog/${blog.id}`}>
+                  <a>{blog.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
 
@@ -69,4 +57,16 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export const getStaticProps = async () => {
+  const blog = await client.get({ endpoint: 'blog' });
+  const tag = await client.get({ endpoint: 'tag' });
+
+  return {
+    props: {
+      blogs: blog.contents,
+      tags: tag.contents,
+    }
+  }
+};
