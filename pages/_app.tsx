0c1c6a38724e6480f2afb0add24989f7578283a3
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2022-12-29 11:50:02 +0900

    setting meta tag.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,14 @@ const PopcornFront = ({ Component, pageProps }: AppPropsWithLayout) => {
     <>
       <Head>
         <title>Kobayashi Blog</title>
+        <meta name="author" content="Kobayashi" />
+        <meta name="Description" content="Kobayashi's blog" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@0719kobayashi" />
+        <meta name="twitter:creator" content="@0719kobayashi" />
+        <meta property="og:site_name" content="Kobayashi's blog" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ja_JP" />
       </Head>
       <Component {...pageProps} />
     </>
